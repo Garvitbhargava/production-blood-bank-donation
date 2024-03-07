9f6fe55f8359e531c86b0da0cfe8e7263418476b
Garvit Bhargava <77270135+Garvitbhargava@users.noreply.github.com>
2024-03-07 16:42:17 +0530

    updated code

---
+++ client/src/components/Shared/Layout/Header.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { BiDonateBlood } from "react-icons/bi";
+import { BiUserCircle } from "react-icons/bi";
+import { useSelector } from "react-redux";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const Header = () => {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  //logout handler
+  const handleLogout = () => {
+    localStorage.clear();
+    toast.success("Logout Successfully");
+    navigate("/login");
+  };
+  return (
+    <>
+      <nav className="navbar bg-dark text-white">
+        <div className="container-fluid">
+          <div className="navbar-brand">
+            <BiDonateBlood color="red" className="mx-3" />
+            Blood Bank App
+          </div>
+          <ul className="navbar-nav flex-row">
+            <li className="nav-item mx-3">
+              <p className="nav-link">
+                <BiUserCircle className="mx-2" />
+                Welcome{" "}
+                {user?.name ||
+                  user?.hospitalName ||
+                  user?.organizationName}{" "}
+                &nbsp;
+                <span className="badge text-bg-secondary">{user?.role}</span>
+              </p>
+            </li>
+            {location.pathname === "/" ||
+            location.pathname === "/donar" ||
+            location.pathname === "/hospital" ? (
+              <li className="nav-item mx-3">
+                <Link to="/analytics" className="nav-link">
+                  Analytics
+                </Link>
+              </li>
+            ) : (
+              <li className="nav-item mx-3">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+            )}
+            <li className="nav-item mx-3">
+              <button className="btn btn-danger" onClick={handleLogout}>
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
---
+++ client/src/services/authService.js
@@ -0,0 +1,47 @@
+import { userLogin, userRegister } from "../redux/features/auth/authActions";
+import store from "../redux/store";
+
+export const handleLogin = (e, email, password, role) => {
+  e.preventDefault();
+  try {
+    if (!role || !email || !password) {
+      return alert("Please Provide all fields");
+    }
+    store.dispatch(userLogin({ email, password, role }));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const handleRegister = (
+  e,
+  name,
+  role,
+  email,
+  password,
+  organizationName,
+  hospitalName,
+  website,
+  address,
+  phone
+) => {
+  e.preventDefault();
+  try {
+    store.dispatch(
+      userRegister({
+        e,
+        name,
+        role,
+        email,
+        password,
+        organizationName,
+        hospitalName,
+        website,
+        address,
+        phone,
+      })
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
